7d7ed96306c349d2dd5dd75d71978da89a7752fa
AlexShevchenko9999 <alexshevchenko9999@gmail.com>
2016-12-05 22:36:28 -0500

    Update SuperArrayIterator.java

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,1 +1,17 @@
-
+public class SuperArrayIterator implements Iterator<String>{
+    SuperArray ary; 
+    int start; 
+  
+    public SuperArrayIterator (SuperArray a){
+        this.ary = a;
+        start = 0;
+    }
+  
+    public boolean hasNext (){
+        return start < ary.isze;
+    }
+  
+    public String next(){
+        if (hasNext()){
+            return ary.get(start);
+        }
