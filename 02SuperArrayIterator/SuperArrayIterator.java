c1b1c30403c6fbdd07054513fed5fd2ee46980fd
Alex Shevchenko <Alex@Alexs-MacBook.local>
2016-12-07 22:05:09 -0500

    Added Barcode.java

---
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,23 @@
+public class SuperArrayIterator implements Iterator<String>{
+    SuperArray ary; 
+    int start; 
+  
+    public SuperArrayIterator (SuperArray a){
+        this.ary = a;
+        start = 0;
+    }
+  
+    public boolean hasNext (){
+        return start < ary.isze;
+    }
+  
+    public String next(){
+        if (hasNext()){
+            return ary.get(start);
+        }
+    }
+    
+    public void remove(){
+           throw new UnsupportedOperationException();
+    }
+}
