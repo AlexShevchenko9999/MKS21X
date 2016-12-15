aac295b3e0ff8403c21e21db63892872a7db59f9
Alex Shevchenko <alexshevchenko9999@gmail.com>
2016-12-15 12:09:54 -0500

    Done

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -10,6 +10,8 @@ public class Sorts{
      *@param data  the elements to be sorted.
      */
 
+    //selectionSort
+
     public static int selectionHelper(int [] data, int x){
 	int ans = x;
 	for (int i = x;i < data.length-1;i++){
@@ -20,9 +22,6 @@ public class Sorts{
 	return ans;
     }
 	  
-	
-    
-    
     public static void selectionSort(int[] data){  
 	for (int i = 0; i < data.length; i++){
 	    int min = data[selectionHelper(data,i)];
@@ -30,6 +29,9 @@ public class Sorts{
 	    data[i] = min;
 	}
     }
+    //=====================================================
+
+    //insertion Sort
 
     public static void insertionHelper(int [] data, int start){
 	for (int i = start-1; i > 0;i--){
@@ -48,13 +50,33 @@ public class Sorts{
 	}
     }
 
+    //=================================================
+
+    //bubbleSort
+
+    public static void bubbleHelper(int [] data, int end){
+	for (int i = 0; i < end; i ++){
+	    if (data[i]>data[i+1]){
+		int replacement = data[i+1];
+		data[i+1]=data[i];
+		data[i] = replacement;
+	    }
+	}
+    }
+
+    public static void bubbleSort(int [] data){
+	for (int i = data.length-1;i > 0  ;i--){
+	    bubbleHelper(data,i);
+	}
+    }
+
 
     public static void main(String [] arg){
 	int [] test = {5,7,3,9,35,456,4,567,34,5678,34,890,-56,56,5893,6};
 	
 	//System.out.println(helper(test,4));
 
-	insertionSort(test);
+	bubbleSort(test);
 	for (int x: test){
 	  System.out.print(x + " ");
 	}
